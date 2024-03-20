de60c026d80021858cc826903533c1571a1ec26e
Jacksix-dev <facundogasco@gmail.com>
2024-03-20 11:28:10 -0300

    added mouse effects

--- src/MouseAnimation/index.js
+++ src/MouseAnimation/index.js
@@ -28,6 +28,8 @@ function MouseAnimation() {
       
       },[])
       
+
+
       const variants ={
         default: {
           x:mousePosition.x - 20,
@@ -38,18 +40,50 @@ function MouseAnimation() {
           y:mousePosition.y - 40,
           height: 100,
           width:100,
-          backgroundColor: 'rgba(33,150,243, 0.3)'
-          
+          border:0,
+          backgroundColor: 'rgba(33,150,243, 0.0)',
+
+          transition: {
+            type: 'tween', // Change the transition type (e.g., spring, tween, etc.)
+            duration: 0.001,   // Change the duration of the transition
+                // Adjust the damping of the spring animation
+          }
         }
       
       }
+
+      const smallVariants ={
+        default: {
+          x:mousePosition.x - 6,
+          y:mousePosition.y - 6
+        },
+        hover:{
+          x:mousePosition.x - 40,
+          y:mousePosition.y - 40,
+          height: 100,
+          width:100,
+          backgroundColor: 'rgba(33,150,243, 0.3)',
+          transition: {
+            type: 'spring', // Change the transition type (e.g., spring, tween, etc.)
+            duration: 2,   // Change the duration of the transition
+            stiffness: 500,  // Adjust the stiffness of the spring animation
+            damping: 30,     // Adjust the damping of the spring animation
+          }
+        }
+      
+      }
+
+
+      const [smallCursorVariant, smallSetCursorVariant] =useState('default')
+
+
       
       const hoverEnter = () =>{setCursorVariant('hover')}
       
       const hoverExit = () =>{ setCursorVariant('default')}
       
       
-  return { variants, cursorVariant, hoverEnter, hoverExit }
+  return {smallCursorVariant, smallVariants, variants, cursorVariant, hoverEnter, hoverExit }
   
 }
 
