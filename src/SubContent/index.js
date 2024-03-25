c9b0592faa8538e77af8f8ee2c5b254ec44e03ac
Jacksix-dev <facundogasco@gmail.com>
2024-03-25 11:50:42 -0300

    added 3d  cards

--- src/SubContent/index.js
+++ src/SubContent/index.js
@@ -1,53 +1,21 @@
 import React from "react";
-import { useState } from "react";
 import { IoMdTimer } from "react-icons/io";
 import { GoDiscussionClosed } from "react-icons/go";
+import { FaRegLightbulb } from "react-icons/fa";
+
 import problemSolving from './solution.png'
-import {motion} from 'framer-motion'
+import { Card } from "./Card";
 
 function SubContent() {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  const handleHover = () => {
-    setIsFlipped(!isFlipped);
-  };
-
+  
   return (
     <>
-    <motion.div>
-      <div
-        className=" w-80 lg:mx-14  h-80 relative"
-        onMouseEnter={handleHover}
-        onMouseLeave={handleHover}
-        style={{ perspective: "1000px" }} 
-      >
-        <motion.div
-          className={`absolute inset-0 w-full h-full bg-sky-800 shadow-md rounded-lg flex flex-col items-center justify-center transform ${
-            isFlipped ? "rotate-y-180" : ""
-          }`}
-          style={{ backfaceVisibility: "hidden" }} // Hide back face of the card
-          initial={{ rotateY: 0 }}
-          animate={{ rotateY: isFlipped ? 180 : 0 }}
-          transition={{ duration: 0.6 }}
-        >
-          <div className="flex-none bg-sky-400 rounded-full w-11 h-11">
-            <IoMdTimer className="text-white"/>
-          </div>
-          <p className="text-2xl">Front</p>
-        </motion.div>
-        <motion.div
-          className={`absolute inset-0 w-full h-full bg-sky-400 shadow-md rounded-lg flex items-center justify-center transform ${
-            isFlipped ? "" : "rotate-y-180"
-          }`}
-          style={{ backfaceVisibility: "hidden" }} // Hide back face of the card
-          initial={{ rotateY: -180 }}
-          animate={{ rotateY: isFlipped ? 0 : -180 }}
-          transition={{ duration: 0.6 }}
-        >
-          <p className="text-2xl">back</p>
-        </motion.div>
-      </div>
-    </motion.div>
+        <div className="flex justify-center space-x-8">
+          <Card frontContent="Exelent at time management" backContent="My time management skills are exceptional, allowing me to handle multiple projects simultaneously without compromising quality." icon={<IoMdTimer size={42} />} />
+          <Card frontContent="Problem-Solving Skills" backContent="I exhibit robust problem-solving abilities, characterized by a systematic approach to analyzing and resolving complex issues. " icon={<FaRegLightbulb  size={42} />} />
+          <Card frontContent="Active Communicator" backContent="I am a proactive communicator, proficient in articulating ideas with clarity and precision in both verbal and written forms." icon={<GoDiscussionClosed size={42} />} />
+
+        </div>
 
     </>
     
